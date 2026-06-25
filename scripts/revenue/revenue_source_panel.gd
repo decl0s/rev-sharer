03c7467ba546609bb1f5d3254da8fb5b22c471bf
Alexandre Declos <alexdeclos@hotmail.com>
2026-06-24 20:09:14 -0400

    feature: payout display on revenue list

--- scripts/revenue/revenue_source_panel.gd
+++ scripts/revenue/revenue_source_panel.gd
@@ -8,4 +8,5 @@ func _on_deletion_requested() -> void:
 
 func init() -> void:
 	%RevenueSourceName.target_resource = revenue
+	%Schedule.target_resource = revenue
 	%AllocatedPercentage.text = Utils.perc_str(Global.get_allocated_percentage(revenue))
