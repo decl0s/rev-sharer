58665634ba6cd91625b77d9619def9f8ab772a1d
Alexandre Declos <alexdeclos@hotmail.com>
2026-06-24 20:42:18 -0400

    feature: revenue source editing

--- scripts/revenue/revenue_source_panel.gd
+++ scripts/revenue/revenue_source_panel.gd
@@ -9,9 +9,18 @@ func _on_deletion_requested() -> void:
 func init() -> void:
 	%RevenueSourceName.target_resource = revenue
 	%Schedule.target_resource = revenue
+	%EditableDescription.target_resource = revenue
 	%AllocatedPercentage.text = Utils.perc_str(Global.get_allocated_percentage(revenue))
+	toggle_hide_desc()
+
+func toggle_hide_desc() -> void:
 	if revenue.description != "":
-		%Description.text = revenue.description
-		%Description.show()
+		%EditableDescription.show()
 	else:
-		%Description.hide()
+		%EditableDescription.hide()
+
+func _on_edit_button_started_editing() -> void:
+	%EditableDescription.show()
+
+func _on_edit_button_ended_editing() -> void:
+	toggle_hide_desc()
